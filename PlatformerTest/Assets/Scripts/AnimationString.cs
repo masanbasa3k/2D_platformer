14d557d270e5aeed7957913b1dce524c9dc456a5
Beyazıt UYSAL <66223190+masanbasa3k@users.noreply.github.com>
2024-10-19 19:28:52 +0300

    game update
    grounds, tile sets, jumping, and jumping animation

---
+++ PlatformerTest/Assets/Scripts/AnimationString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class AnimationString
+{
+    internal static string isMoving = "isMoving";
+    internal static string isRunning = "isRunning";
+    internal static string isGrounded = "isGrounded";
+    internal static string yVelocity = "yVelocity";
+    internal static string jump = "jump";
+    internal static string IsOnWall = "isOnWall";
+    internal static string IsOnCeiling = "isOnCeiling";
+}
--- PlatformerTest/Assets/Scripts/PlayerController.cs
+++ PlatformerTest/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirection))]
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 3f;
     public float runSpeed = 7f;
+    public float airWalkSpeed = 5f;
+    public float jumpImpulse = 10f;
     Vector2 moveInput;
+    TouchingDirection touchingDirection;
 
     public float moveSpeed 
     { 
         get 
         {
-            if (IsMoving)
+            if (IsMoving && !touchingDirection.IsOnWall)
             {
-                if (IsRunning)
-                { return runSpeed; }
-                else { return walkSpeed; }
+                if (touchingDirection.IsGrounded)
+                {
+                    if (IsRunning)
+                    { return runSpeed; }
+                    else { return walkSpeed; }
+                }
+                else
+                {
+                    return airWalkSpeed;
+                }
             }
             else
             {
@@ -41,7 +51,7 @@ public class PlayerController : MonoBehaviour
         private set
         {
             _isMoving = value;
-            animator.SetBool("isMoving", value);
+            animator.SetBool(AnimationString.isMoving, value);
         }
     }
 
@@ -57,7 +67,7 @@ public class PlayerController : MonoBehaviour
         set
         {
             _isRunning = value;
-            animator.SetBool("isRunning", value);
+            animator.SetBool(AnimationString.isRunning, value);
         }
     }
 
@@ -86,11 +96,14 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        touchingDirection  = GetComponent<TouchingDirection>();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+
+        animator.SetFloat(AnimationString.yVelocity, rb.velocity.y);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -123,6 +136,16 @@ public class PlayerController : MonoBehaviour
         else if (context.canceled)
         {
             IsRunning = false;
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        // todo check player is alive
+        if (context.started && touchingDirection.IsGrounded)
+        {
+            animator.SetTrigger(AnimationString.jump);
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
         }
     }
 }
